37fb1766185bc97090b44727c50c776cf4516c5c
Luis Villén <l.villenjaldon@gmail.com>
2021-01-13 20:44:23 +0100

    Sumamos explosión

--- Arkanoid/entidades.py
+++ Arkanoid/entidades.py
@@ -1,7 +1,7 @@
 import pygame as pg
 import sys
 import random
-from Arkanoid import GAME_DIMENSIONS
+from Arkanoid import GAME_DIMENSIONS, FPS
 
 
 
@@ -10,6 +10,7 @@ pg.init()
 
 class Pelota:
     imagenes_files = ['brown_ball.png', 'blue_ball.png', 'red_ball.png', 'green_ball.png']
+    num_imgs_explosion = 8
 
     def __init__(self, x, y, vx, vy):
         self.x = x
@@ -19,8 +20,16 @@ class Pelota:
         self.imagen_act = 0
         self.imagenes = self.cargaImagenes()
         self.imagen = self.imagenes[self.imagen_act]
+        self.imagenes_explosion = self.cargaExplosion()
+        self.ix_explosion = 0
+        self.muriendo = False
 
 
+    def cargaExplosion(self):
+        return [pg.image.load(f"resources/images/explosion0{i}.png") for i in range(self.num_imgs_explosion)]
+        
+    # cargaExplosion y cargaImagenes son dos formas de hacer lo mismo.
+        
     def cargaImagenes(self):
         lista_imagenes = []
         for img in self.imagenes_files:
@@ -28,36 +37,68 @@ class Pelota:
         
         return lista_imagenes
 
-
     @property
     def rect(self):
         return self.imagen.get_rect(topleft=(self.x, self.y))
 
-    def actualizar(self):
+
+    def actualizar_posicion(self):
         #Gestionar posición de pelota
+        if self.muriendo:
+            return
+        
         if self.rect.left <= 0 or self.rect.right >= GAME_DIMENSIONS[0]:
             self.vx = -self.vx
-        if self.rect.top <= 0 or self.rect.bottom >= GAME_DIMENSIONS[1]:
+            self.actualizar_disfraz()
+
+        if self.rect.top <= 0: 
             self.vy = -self.vy
+            self.actualizar_disfraz()
+
+        if self.rect.bottom >= GAME_DIMENSIONS[1]:
+            self.muriendo = True
+            return
+
         self.x += self.vx
         self.y += self.vy
 
+    def actualizar_disfraz(self):
         #Gestionar imagen activa (disfraz) de la pelota
         self.imagen_act += 1
         if self.imagen_act >= len(self.imagenes):
             self.imagen_act = 0
         self.imagen = self.imagenes[self.imagen_act]
 
+    def actualizar(self):
+        self.actualizar_posicion()
+
+        if self.muriendo:
+            self.explosion()
+        #else: 
+            #self.actualizar_disfraz() si quisiéramos que se quedase aquí
+
+    def explosion(self):
+        if self.ix_explosion >= len(self.imagenes_explosion):
+            #terminar programa 
+            self.ix_explosion = 0
+
+        self.imagen = self.imagenes_explosion[self.ix_explosion]
+        self.ix_explosion += 1
+
+
+
 class Game:
     def __init__(self):
         self.pantalla = pg.display.set_mode(GAME_DIMENSIONS)
         pg.display.set_caption("Futuro Arkanoid")
         self.pelota = Pelota(400, 300, 10, 10)
+        self.clock = pg.time.Clock()
         #TODO Background
 
     def main_loop(self):
         game_over = False
         while not game_over:
+            print(self.clock.tick(FPS))
             events = pg.event.get()
             for event in events:
                 if event.type == pg.QUIT:
--- main.py
+++ main.py
@@ -3,4 +3,3 @@ from Arkanoid.entidades import *
 if __name__ == "__main__":
     game = Game()
     game.main_loop()
-
